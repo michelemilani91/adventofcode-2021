2f5c1be0999554c83996164a5fce70fcb2a1502e
michele.milani@facile.it <naqsi5-Joxnud-zutwoz>
2021-12-09 16:52:00 +0100

    day 3 - part two

--- src/day-3/index.ts
+++ src/day-3/index.ts
@@ -16,30 +16,25 @@ interface BinaryCounter {
     numberOf1: number;
 }
 
-const countBitsPerColumn = (a: readonly BinaryNumber[]) => {
-    return a.reduce<BinaryCounter[]>((counter, value) => {
-        return value.map<BinaryCounter>((bit, index) => {
-            const previousValue = counter[index] || { numberOf0: 0, numberOf1: 0 };
-            switch (bit) {
-                case '0':
-                    previousValue.numberOf0++;
-                    break;
-                case '1':
-                    previousValue.numberOf1++;
-                    break;
-            }
-            return previousValue;
-        });
-    }, []);
-};
+const countBitsPerColumn = (a: readonly BinaryNumber[]): readonly BinaryCounter[] =>
+    a.reduce<BinaryCounter[]>((counter, value) =>
+            value.map<BinaryCounter>((bit, index) => {
+                const previousValue = counter[index] || { numberOf0: 0, numberOf1: 0 };
+                switch (bit) {
+                    case '0':
+                        previousValue.numberOf0++;
+                        break;
+                    case '1':
+                        previousValue.numberOf1++;
+                        break;
+                }
+                return previousValue;
+            })
+        , []);
 
-const calculateGammaRate = (a: readonly BinaryCounter[]) => {
-    return Number.parseInt(a.map(x => x.numberOf0 > x.numberOf1 ? '0' : '1').join(''), 2);
-}
+const calculateGammaRate = (a: readonly BinaryCounter[]): number => Number.parseInt(a.map(x => x.numberOf0 > x.numberOf1 ? '0' : '1').join(''), 2);
 
-const calculateEpsilonRate = (a: readonly BinaryCounter[]) => {
-    return Number.parseInt(a.map(x => x.numberOf0 < x.numberOf1 ? '0' : '1').join(''), 2);
-}
+const calculateEpsilonRate = (a: readonly BinaryCounter[]): number => Number.parseInt(a.map(x => x.numberOf0 < x.numberOf1 ? '0' : '1').join(''), 2);
 
 export const partOne = flow(
     readFile,
@@ -48,10 +43,39 @@ export const partOne = flow(
     TE.map((a: readonly string[][]) => a.map(parseBinaryNumber)),
     TE.chainEitherKW(E.sequenceArray),
     TE.map(countBitsPerColumn),
-    TE.map((a: readonly BinaryCounter[]) => calculateGammaRate(a) * calculateEpsilonRate(a))
+    TE.map((a: readonly BinaryCounter[]) => calculateGammaRate(a) * calculateEpsilonRate(a)),
 );
 
 
 partOne('src/day-3/input.txt')()
     .then(a => E.isRight(a) ? console.log('Part one: What is the power consumption of the submarine? ', a.right) : console.log('Error: ', a.left))
+    .catch(a => console.log('Error: ', a));
+
+const calculateRating = (array: readonly BinaryNumber[], evaluatedValue: '0' | '1', column: number = 0): BinaryNumber => {
+    const { numberOf1, numberOf0 } = countBitsPerColumn(array)[column];
+    const bitValue = evaluatedValue == '1' ? numberOf1 >= numberOf0 : numberOf1 < numberOf0;
+    const filteredArray = array.filter(a => Boolean(Number(a[column])) === bitValue);
+    return filteredArray.length === 1 ? filteredArray[0] : calculateRating(filteredArray, evaluatedValue, column + 1);
+};
+
+const calculateOxygenGeneratorRating = (a: readonly BinaryNumber[]): number => {
+    return Number.parseInt(calculateRating(a, '1').join(''), 2);
+}
+
+const calculateCO2ScrubberRating = (a: readonly BinaryNumber[]): number => {
+    return Number.parseInt(calculateRating(a, '0').join(''), 2);
+}
+
+export const partTwo = flow(
+    readFile,
+    TE.map((a: Readonly<string>) => a.split("\n")),
+    TE.map(A.map((b: string) => b.split(''))),
+    TE.map((a: readonly string[][]) => a.map(parseBinaryNumber)),
+    TE.chainEitherKW(E.sequenceArray),
+    TE.map((a: readonly BinaryNumber[]) => calculateOxygenGeneratorRating(a) * calculateCO2ScrubberRating(a))
+);
+
+
+partTwo('src/day-3/input.txt')()
+    .then(a => E.isRight(a) ? console.log('Part two: What is the life support rating of the submarine? ', a.right) : console.log('Error: ', a.left))
     .catch(a => console.log('Error: ', a));
--- test/day-3/index.spec.ts
+++ test/day-3/index.spec.ts
@@ -1,10 +1,15 @@
 import * as E from "fp-ts/Either";
-import { partOne } from "@app/day-3";
+import {partOne, partTwo} from "@app/day-3";
 
 describe("AdventOfCode 2021 - Day 3", () => {
     it("Return result 198 for first challenge", async () => {
         const result = await partOne('test/day-3/input.txt')();
         const expected = E.right(198);
+        expect(result).toEqual(expected);
+    });
+    it("Return result 230 for second challenge", async () => {
+        const result = await partTwo('test/day-3/input.txt')();
+        const expected = E.right(230);
         expect(result).toEqual(expected);
     });
 });
