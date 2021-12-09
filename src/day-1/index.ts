f5c0e809983c51837bb7f9645b4ff4e54cb76b4a
michele.milani@facile.it <naqsi5-Joxnud-zutwoz>
2021-12-09 01:22:56 +0100

    day 1 - part two

--- src/day-1/index.ts
+++ src/day-1/index.ts
@@ -3,13 +3,29 @@ import * as TE from "fp-ts/TaskEither";
 import * as E from "fp-ts/Either";
 import { readFile } from "@app/utils/file";
 
-export const main = flow(
+function countIncreases(a: number[]) {
+    return a.filter((value, index, array) => index - 1 >= 0 && value > array[index - 1]).length;
+}
+
+export const partOne = flow(
     readFile,
     TE.map((a: string) => a.split("\n")),
-    TE.map((a: string[]) => a.map((x) => Number.parseInt(x))),
-    TE.map((a: number[]) => a.filter((value, index, array) => index-1 >= 0 && value > array[index-1]).length)
+    TE.map((a: string[]) => a.map((value) => Number.parseInt(value))),
+    TE.map(countIncreases)
 );
 
-main('src/day-1/input.txt')()
+partOne('src/day-1/input.txt')()
     .then(a => E.isRight(a) ? console.log('How many measurements are larger than the previous measurement? ', a.right) : console.log('Error: ', a.left))
+    .catch(a => console.log('Error: ', a));
+
+export const partTwo = flow(
+    readFile,
+    TE.map((a: string) => a.split("\n")),
+    TE.map((a: string[]) => a.map((value) => Number.parseInt(value))),
+    TE.map((a: number[]) => a.map((value, index, array) => value + array[index-1] + array[index-2]).slice(2)),
+    TE.map(countIncreases)
+);
+
+partTwo('src/day-1/input.txt')()
+    .then(a => E.isRight(a) ? console.log('How many sums are larger than the previous sum? ', a.right) : console.log('Error: ', a.left))
     .catch(a => console.log('Error: ', a));
--- test/day-1/index.spec.ts
+++ test/day-1/index.spec.ts
@@ -1,17 +1,22 @@
 import * as E from "fp-ts/Either";
-import { main } from "@app/day-1";
+import { partOne, partTwo } from "@app/day-1";
 
 describe("AdventOfCode 2021 - Day 1", () => {
   it("Return error for non existing file", async () => {
-    const result = await main('src/day-1/non-exists.txt')();
-    const expected = E.left("ENOENT: no such file or directory, open 'src/day-1/non-exists.txt'");
+    const result = await partOne('test/day-1/non-exists.txt')();
+    const expected = E.left("ENOENT: no such file or directory, open 'test/day-1/non-exists.txt'");
 
     expect(result).toEqual(expected);
   });
-
   it("Return result 7 for first challenge", async () => {
-    const result = await main('test/day-1/input-1.txt')();
+    const result = await partOne('test/day-1/input.txt')();
     const expected = E.right(7);
+
+    expect(result).toEqual(expected);
+  });
+  it("Return result 5 for second challenge", async () => {
+    const result = await partTwo('test/day-1/input.txt')();
+    const expected = E.right(5);
 
     expect(result).toEqual(expected);
   });
