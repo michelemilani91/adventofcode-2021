79b3b60199b98ec5354a7860160ba78dd6441a58
michele.milani@facile.it <naqsi5-Joxnud-zutwoz>
2021-12-10 02:22:37 +0100

    day 4 - part two

--- src/day-4/index.ts
+++ src/day-4/index.ts
@@ -21,43 +21,64 @@ const parseFileContent = (input: string): GameData => {
     };
 }
 
-interface ProcessingGame {
-    boards: number[][][];
-    winnerNumber?: number;
-    index: number;
-    sum?: number;
-}
-
 const transpose = (matrix: number[][]) => {
     return matrix[0].map((_col, i) => matrix.map(row => row[i]));
 }
-const processDataPerRow = (gameData: GameData) => gameData.sequence.reduce<ProcessingGame>((acc, number, index) => {
-    if (acc.winnerNumber !== undefined) return acc;
-    acc.boards = acc.boards.map(board => board.map(row => row.filter(value => value !== number)));
-    const boardsWithEmptyRow = acc.boards.find(board => board.find(row => !row.length));
-    if (index >= 5 && boardsWithEmptyRow) {
-        acc.winnerNumber = number;
-        acc.index = index;
-        acc.sum = boardsWithEmptyRow.reduce((sum, row) => row.reduce((partialSum, value) => partialSum + value, sum), 0);
-    }
-    return acc;
-}, { boards: gameData.boards, index: -1 });
 
-function processData(gameData: GameData) {
-    const processingGameRows = processDataPerRow(gameData);
-    const processingGameColumn = processDataPerRow({...gameData, boards: gameData.boards.map(transpose)});
-    return processingGameRows.index < processingGameColumn.index ? processingGameRows : processingGameColumn;
+const boardToPossibilities = (board: number[][]) => [ ...board, ...transpose(board) ];
+
+interface TurnCount {
+    reducedBoard: number[][];
+    index: number;
+    sequenceValue: number;
 }
 
-const multiplyNumberBySum = (p: ProcessingGame) => (p.winnerNumber || 0) * (p.sum || 0);
+const countTurnsToGetEmptyRow = (sequence: number[], board: number[][], index: number = 0, sequenceValue: number = -1): TurnCount => {
+    if (sequence.length === 0) return { reducedBoard: board, index, sequenceValue };
+    const [ nextVal, ...otherPartOfSequence ] = sequence;
+    const reducedBoard = board.map(row => row.filter(v => v !== nextVal));
+    const newSequence = reducedBoard.find(p => p.length === 0) ? [] : otherPartOfSequence;
+    return countTurnsToGetEmptyRow(newSequence, reducedBoard, index + 1, nextVal);
+}
+
+const toTurnCounts = (gameData: GameData): TurnCount[] => gameData.boards.map(board => countTurnsToGetEmptyRow(gameData.sequence, boardToPossibilities(board)));
+
+const findFirstWinnerBoard = (turnCounts: TurnCount[]): TurnCount => turnCounts.reduce((acc, val) => acc.index < val.index ? acc : val);
+
+interface ProcessingResult {
+    sum: number;
+    sequenceValue: number;
+}
+
+const processData = ({ reducedBoard, sequenceValue }: TurnCount): ProcessingResult => ({ sequenceValue, sum: reducedBoard.slice(0, reducedBoard.length / 2).reduce((sum, row) => row.reduce((rowSum, val) => rowSum + val, sum), 0) });
+
+const multiplyNumberBySum = (p: ProcessingResult) => p.sequenceValue * p.sum;
 
 export const partOne = flow(
     readFile,
     TE.map(parseFileContent),
+    TE.map(toTurnCounts),
+    TE.map(findFirstWinnerBoard),
     TE.map(processData),
-    TE.map((p: ProcessingGame) => multiplyNumberBySum(p)),
+    TE.map(multiplyNumberBySum),
 );
 
 partOne('src/day-4/input.txt')()
     .then(a => E.isRight(a) ? console.log('Part one: What will your final score be if you choose that board? ', a.right) : console.log('Error: ', a.left))
+    .catch(a => console.log('Error: ', a));
+
+
+const findLastWinnerBoard = (turnCounts: TurnCount[]): TurnCount => turnCounts.reduce((acc, val) => acc.index > val.index ? acc : val);
+
+export const partTwo = flow(
+    readFile,
+    TE.map(parseFileContent),
+    TE.map(toTurnCounts),
+    TE.map(findLastWinnerBoard),
+    TE.map(processData),
+    TE.map(multiplyNumberBySum),
+);
+
+partTwo('src/day-4/input.txt')()
+    .then(a => E.isRight(a) ? console.log('Part two: Once it wins, what would its final score be? ', a.right) : console.log('Error: ', a.left))
     .catch(a => console.log('Error: ', a));
--- test/day-4/index.spec.ts
+++ test/day-4/index.spec.ts
@@ -1,10 +1,15 @@
 import * as E from "fp-ts/Either";
-import {partOne} from "@app/day-4";
+import {partOne, partTwo} from "@app/day-4";
 
 describe("AdventOfCode 2021 - Day 4", () => {
     it("Return result 4512 for first challenge", async () => {
         const result = await partOne('test/day-4/input.txt')();
         const expected = E.right(4512);
+        expect(result).toEqual(expected);
+    });
+    it("Return result 1924 for second challenge", async () => {
+        const result = await partTwo('test/day-4/input.txt')();
+        const expected = E.right(1924);
         expect(result).toEqual(expected);
     });
 });
